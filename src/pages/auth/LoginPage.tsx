542d84c0af3cc705c93e7a0bcdb8fe93d49e06d2
jungho-Kang <rkdwjdgh08@gmail.com>
2026-06-25 11:30:22 +0900

    [feat] 회원가입 구현

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,6 +1,14 @@
 import Login from "@/components/auth/Login";
+import SignUp from "@/components/auth/SignUp";
+import { useState } from "react";
+
+type Mode = "login" | "signup";
 
 function LoginPage() {
+  const [mode, setMode] = useState<Mode>("login");
+
+  // TODO: 이메일 인증 기능 생기면 회원가입 폼을 모달창으로 바꾸고 API 붙이기
+  // 모달창을 알파스퀘어 방식으로 적용 (Figma 참고)
   return (
     <div className="min-h-screen flex items-center justify-center px-4 bg-[#0b0d12] relative overflow-hidden">
       <div
@@ -30,24 +38,24 @@ function LoginPage() {
           </p>
 
           {/* 로그인 폼 */}
-          <main>
-            <Login />
-          </main>
+          <main>{mode === "login" ? <Login /> : <SignUp />}</main>
 
           {/* 회원가입 텍스트 링크 */}
-          <p className="mt-6 text-center text-[13px] text-gray-600">
-            계정이 없으신가요?{" "}
-            <button
-              type="button"
-              onClick={() => {}}
-              className="text-[#9B7BFF] underline underline-offset-2
+          {mode === "login" && (
+            <p className="mt-6 text-center text-[13px] text-gray-600">
+              계정이 없으신가요?{" "}
+              <button
+                type="button"
+                onClick={() => setMode("signup")}
+                className="text-[#9B7BFF] underline underline-offset-2
                          hover:text-[#B794F4] transition-colors duration-200
                          focus-visible:outline-none focus-visible:rounded-sm
                          focus-visible:ring-2 focus-visible:ring-[#7C5CFF] cursor-pointer"
-            >
-              회원가입
-            </button>
-          </p>
+              >
+                회원가입
+              </button>
+            </p>
+          )}
         </div>
       </div>
     </div>
